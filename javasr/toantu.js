23e8c82d0c9232e0b18219ef116f3dea01c05b26
NgoVietTrung <0611vietttrung@gmail.com>
2022-05-09 08:59:50 +0700

    add

---
+++ javasr/toantu.js
@@ -0,0 +1,8 @@
+let inputwidth;
+let inputheigth;
+inputwidth=prompt('Nhập width');
+inputheigth=prompt('Nhập heigth');
+let width=parseInt(inputwidth);
+let height=parseInt(inputheigth);
+let area=width*height;
+document.write('area=' +area)
